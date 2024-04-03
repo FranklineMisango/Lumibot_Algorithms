3973d1722c1733dceca5c66ca00fa5966c91c9ff
misango <franklinemisango@gmail.com>
2024-04-03 11:00:02 +0800

    fixed the date and stock segmentations

--- algo.py
+++ algo.py
@@ -17,16 +17,15 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use("agg") 
-from Algorithms import buy_hold
-from Algorithms import single_stock
-from Algorithms import swing_high
-from Algorithms import trend 
+
 
 st.set_page_config(layout="wide")
 st.title("🦜🔗 Algorithmic Trading Framework using Lumibots API")
-message =  "This project is intended for users with an intermediate knowledge of Finance"
+message =  "This project is intended for users with some knowledge in Finance and stats. Please read the documentation to understand. Please Read the documentation to understand what each platform does technically"
 st.warning(message)
-
+global start_date
+global end_date
+global portfolio_size
 
 def main():
 
@@ -43,9 +42,9 @@ def main():
     with st.form(key='ticker_form'):
         col1, col2, col3 = st.columns([4, 4, 2])  # Adjust column widths as needed
         with col1:
-            ticker_input = st.text_input("Enter a stock ticker:", key='ticker_input')
+            ticker_input = st.text_input("Enter a stock ticker: ", key='ticker_input')
         with col2:
-            quantities_input = st.number_input("Enter the stock quantity")
+            quantities_input = st.number_input("Enter the stock quantity: ")
         with col3:
             add_ticker_button = st.form_submit_button(label="Add +")
             delete_quantity_button = st.form_submit_button(label="Delete -")
@@ -77,44 +76,52 @@ def main():
         for ticker, quantity in zip(st.session_state.tickers, st.session_state.quantities):
             st.write(f"{ticker}: {quantity}")
         
-        global start_date
-        global end_date
-        global portfolio_size
-        # Add the Start Date and End Date
-        with st.form(key='start_end_dates'):
-            st.header("Backtesting Strategies using Lumibots API")
-            st.warning("Read the documentation to understand what each platform does technically")
-            portfolio_size = st.number_input("Enter the value of your portfolio in ($):")
-            try:
-                val = float(portfolio_size)
-            except ValueError:
-                print("That's not a number! \n Try again:")
-                portfolio_size = input("Enter the value of your portfolio in ($):")
-            option = st.radio(
-                                'Please select the Strategy/services you would like to use (more under development);', (
-                                'Buy & Hold (Strategy & Backtesting)' , "Swing High(Strategy)", "Trend", "GLD signal", "Portfolio earnings", "single stock earnings"
+
+    
+def dates():
+    # Add the Start Date and End Date
+    with st.form(key='start_end_dates'):
+        portfolio_size = st.number_input("Enter the value of your portfolio in ($):")
+        try:
+            val = float(portfolio_size)
+        except ValueError:
+            print("That's not a number! \n Try again:")
+            portfolio_size = input("Enter the value of your portfolio in ($):")
+    
+        col1, col2 = st.columns([2, 2])
+        with col1:
+            start_date = st.date_input("Start date:")
+        with col2:
+            end_date = st.date_input("End Date:")
+        if st.form_submit_button("Submit"):
+             st.success("Recorded the start and end dates. Proceed to the strategies") 
+def carry_out_strategies():
+    #Add the Lumibot strategies
+    with st.form(key="strategies"):
+        st.header("Backtesting and Trading Strategies using Lumibots API")
+        option = st.radio(
+                                    'Please select the Strategy/services you would like to use (more under development);', (
+                                    'Buy & Hold (Strategy & Backtesting)' , "Swing High(Strategy)", "Trend", "GLD signal", "Portfolio earnings", "single stock earnings"
+                                    )
                                 )
-                            )
-            col1, col2 = st.columns([2, 2])
-            with col1:
-                start_date = st.date_input("Start date:")
-            with col2:
-                end_date = st.date_input("End Date:")
-        
-            if option == "Buy & Hold (Strategy & Backtesting)":
+        if option == "Buy & Hold (Strategy & Backtesting)":
+                pass           
+        if option == "Swing High":
                 pass
-            if option == "Swing High":
+        if option == "ma-cross strategy":
                 pass
-            if option == "ma-cross strategy":
-                pass
-            if option == "Trend":
+        if option == "Trend":
                 pass            
-            if option == "GLD signal":
+        if option == "GLD signal":
                 pass   
-            if option == "Portfolio earnings":
+        if option == "Portfolio earnings":
                 pass
-            if option == "single stock backtest":
+        if option == "single stock backtest":
                 pass
-                
+        
+        st.form_submit_button("Submit")
+
                 
 main()
+dates()
+carry_out_strategies()
