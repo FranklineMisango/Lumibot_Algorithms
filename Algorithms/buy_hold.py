d17600b16e37e70b5c423211f2826e3ce3530d78
misango <franklinemisango@gmail.com>
2024-09-10 22:30:39 +0800

    test_warren_buy_hold strategy

--- Algorithms/buy_hold.py
+++ Algorithms/buy_hold.py
@@ -24,7 +24,42 @@ class BuyHold(Strategy):
     def on_trading_iteration(self):
         if self.first_iteration:
             stocks_and_quantities = [
-                {"symbol": 'NVDA', "quantity": 100},
+                {"symbol": "ALLY", "quantity": 290},
+                {"symbol": "AMZN", "quantity": 100},
+                {"symbol": "AXP", "quantity": 75},
+                {"symbol": "AON", "quantity": 410},
+                {"symbol": "AAPL", "quantity": 400},
+                {"symbol": "BATRK", "quantity": 223},
+                {"symbol": "BAC", "quantity": 90},
+                {"symbol": "COF", "quantity": 70},
+                {"symbol": "CHTR", "quantity": 65},
+                {"symbol": "CVX", "quantity": 110},
+                {"symbol": "C", "quantity": 95},
+                {"symbol": "CB", "quantity": 85},
+                {"symbol": "DVA", "quantity": 45},
+                {"symbol": "DEO", "quantity": 100},
+                {"symbol": "FND", "quantity": 40},
+                {"symbol": "JEF", "quantity": 25},
+                {"symbol": "KHC", "quantity": 35},
+                {"symbol": "KR", "quantity": 50},
+                {"symbol": "LILA", "quantity": 70},
+                {"symbol": "LILAK", "quantity": 80},
+                {"symbol": "LSXMA", "quantity": 90},
+                {"symbol": "LSXMK", "quantity": 100},
+                {"symbol": "FWONK", "quantity": 130},
+                {"symbol": "LPX", "quantity": 140},
+                {"symbol": "MA", "quantity": 150},
+                {"symbol": "MCO", "quantity": 160},
+                {"symbol": "NU", "quantity": 170},
+                {"symbol": "NVR", "quantity": 180},
+                {"symbol": "OXY", "quantity": 190},
+                {"symbol": "SIRI", "quantity": 200},
+                {"symbol": "SPY", "quantity": 210},
+                {"symbol": "TMUS", "quantity": 220},
+                {"symbol": "ULTA", "quantity": 230},
+                {"symbol": "VOO", "quantity": 240},
+                {"symbol": "VRSN", "quantity": 250},
+                {"symbol": "SNOW", "quantity": 260},
             ]
             for stock_info in stocks_and_quantities:
                 symbol = stock_info["symbol"]
@@ -35,7 +70,7 @@ class BuyHold(Strategy):
                     order = self.create_order(symbol, quantity, "buy")
                     self.submit_order(order)
 
-trade = False
+trade = True
 if trade:
     broker = Alpaca(ALPACA_CONFIG)
     strategy = BuyHold(broker=broker)
