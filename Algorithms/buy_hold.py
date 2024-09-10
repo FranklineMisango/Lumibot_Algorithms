e7d283ccbb1f1cd427b4f190464b6d2d6465671a
misango <franklinemisango@gmail.com>
2024-09-10 14:11:39 +0800

    fixed buy/hold

--- Algorithms/buy_hold.py
+++ Algorithms/buy_hold.py
@@ -2,29 +2,39 @@ from lumibot.brokers import Alpaca
 from lumibot.strategies import Strategy
 from lumibot.traders import Trader
 import yfinance as yf
-import datetime 
+from datetime import datetime
 from lumibot.backtesting import YahooDataBacktesting
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
+# Populate the ALPACA_CONFIG dictionary
+ALPACA_CONFIG = {
+    'API_KEY': os.environ.get('APCA_API_KEY_ID'),
+    'API_SECRET': os.environ.get('APCA_API_SECRET_KEY'),
+    'BASE_URL': os.environ.get('BASE_URL')
+}
 
 class BuyHold(Strategy):
     def initialize(self):
         self.sleeptime = "1D"
+        self.set_cash = 100000  # Set initial cash balance
 
     def on_trading_iteration(self):
         if self.first_iteration:
             stocks_and_quantities = [
-                {"symbol": ticker_input, "quantity": quantities_input},
+                {"symbol": 'NVDA', "quantity": 100},
             ]
             for stock_info in stocks_and_quantities:
                 symbol = stock_info["symbol"]
                 quantity = stock_info["quantity"]
                 price = self.get_last_price(symbol)
                 cost = price * quantity
-                self.cash = portfolio_size
                 if self.cash >= cost:
                     order = self.create_order(symbol, quantity, "buy")
                     self.submit_order(order)
+
 trade = False
 if trade:
     broker = Alpaca(ALPACA_CONFIG)
@@ -33,8 +43,8 @@ if trade:
     trader.add_strategy(strategy)
     trader.run_all()
 else:
-    start = datetime(start_date.year, start_date.month, start_date.day)  # Convert start_date to datetime
-    end = datetime(end_date.year, end_date.month, end_date.day)  # Convert end_date to datetime
+    start = datetime(2024, 8, 1)  # Convert start_date to datetime
+    end = datetime(2024, 8, 31)  # Convert end_date to datetime
     BuyHold.backtest(
         YahooDataBacktesting,
         start,
