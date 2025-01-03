991b3275e3e5dba9d4de8ce6d838b8dac2011f43
misango <franklinemisango@gmail.com>
2025-01-03 17:01:16 +0300

    live SPY run with alpaca mkt signals

--- Portfolio_Strategies/Astral_Signals_Hold_short_strategy/live_astral_signals.py
+++ Portfolio_Strategies/Astral_Signals_Hold_short_strategy/live_astral_signals.py
@@ -1,6 +1,8 @@
 import yfinance as yf
 import plotly.graph_objects as go
 import datetime as dt
+import alpaca_trade_api as tradeapi
+
 
 # Universal Helper function for the Astral Signals strategy : DON'T MODIFY
 def astral(data, completion, step, step_two, what, high, low, where_long, where_short):
@@ -9,6 +11,7 @@ def astral(data, completion, step, step_two, what, high, low, where_long, where_
 
     # Iterate through the DataFrame
     for i in range(len(data)):
+
         # Long signal logic
         if data.iloc[i][what] < data.iloc[i - step][what] and data.iloc[i][low] < data.iloc[i - step_two][low]:
             data.at[data.index[i], 'long_signal'] = -1
@@ -25,6 +28,22 @@ def astral(data, completion, step, step_two, what, high, low, where_long, where_
 
 
 def main(start_date : dt.time, end_date : dt.time, ticker:str):
+
+    # Create an async function to run between 9.30AM and 3.45 PM for a certain ticker
+
+
+    # Formulate a function to check the last 1 hour of ticker movement and place buy/sell orders
+
+
+    # If dynamic movement for the one hour is more than 10% , buy more of that ticker
+
+
+    # If a certain ticker loses 90% of value within 1 hr, sell it 
+
+
+    #validate our PnL  and alert the client of the mkt mvts
+
+
     ''' This program allows you to view the Astral Signals of a ticker over time. '''
     start = start_date
     end = end_date
@@ -62,4 +81,7 @@ def main(start_date : dt.time, end_date : dt.time, ticker:str):
 
 
 if __name__ == '__main__':
+
+    # Replace with the async function 
+    
     main(dt.datetime(2024, 10, 21, 9, 30), dt.datetime(2024, 10, 21, 16, 0), 'AAPL')
